27f3dc94c0163c2fd734b74c9344070fe13fbced
frankwwu <frankwwu@hotmail.com>
2015-04-01 20:30:39 -0400

    Global Commands

---
+++ GlobalCommands/WpfGlobalCommands/GlobalCommands.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace WpfGlobalCommands
+{
+    public static class GlobalCommands 
+    {
+        public static ICommand ChangeSettingCommand = new DelegateCommand<string>(ChangeSetting, CanChangeSetting);
+
+        public static void ChangeSetting(string param)
+        {
+            MessageBox.Show(param, "Global Command Demo");
+        }
+
+        private static bool CanChangeSetting(string param)
+        {
+            return true;
+        }
+    }
+}

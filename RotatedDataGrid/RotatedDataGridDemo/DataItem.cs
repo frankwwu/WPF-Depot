dc99c71298de3992b6c27a3b65e21a7c7d165626
frankwwu <frankwwu@hotmail.com>
2015-06-06 11:19:42 -0400

    Rotated DataGrid

---
+++ RotatedDataGrid/RotatedDataGridDemo/DataItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RotatedDataGridDemo
+{   
+    public class DataItem
+    {           
+        public string Name { get; set; }
+ 
+        public string Status { get; set; }
+
+        public int Analysis { get; set; }
+
+        public int Rate { get; set; }
+
+        public int Review { get; set; }
+     
+        public DateTime DateTime { get; set; }
+
+        public string Version { get; set; }
+
+        public string Comment { get; set; }       
+    }
+}
---
+++ RotatedDataGrid/RotatedDataGridDemo/ViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace RotatedDataGridDemo
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public ViewModel()
+        {          
+            Data = new List<DataItem>();
+            DataItem item = new DataItem();
+            item.DateTime = DateTime.Now;
+            item.Name = "American Software Bug Insurance";
+            item.Analysis = 5;
+            item.Rate = 8;
+            item.Review = 185;
+            item.Status = "Final";
+            item.Version = "2.0";
+            item.Comment = "Software bugs can cost companies billions of dollars in repairs, lawsuits, and sales lost.";
+            Data.Add(item);
+        }
+
+        public List<DataItem> Data { get; set; }
+
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}

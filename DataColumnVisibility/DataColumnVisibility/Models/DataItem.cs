be2b66d0956fb2000775f9e6a137ed037f237c7d
frankwwu <frankwwu@hotmail.com>
2016-10-19 19:13:20 -0400

    DataColumnVisibility

---
+++ DataColumnVisibility/DataColumnVisibility/Models/DataItem.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+
+namespace DataColumnVisibility.Models
+{
+    public class DataItem : INotifyPropertyChanged
+    {
+        private bool isEditable;
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsEditable
+        {
+            get
+            {
+                return isEditable;
+            }
+            set
+            {
+                isEditable = value;
+                NotifyPropertyChanged("IsEditable");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

bfd1c1df526b4b0c249db192e680c19991fa0947
frankwwu <frankwwu@hotmail.com>
2016-10-20 19:04:24 -0400

    DataColumnVisibility

---
+++ DataColumnVisibility/DataColumnVisibility/Services/DataService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel.Composition;
+using DataColumnVisibility.Models;
+
+namespace DataColumnVisibility.Services
+{
+    /// <summary>
+    /// Dummy data service class. Provides a dummy data model.
+    /// </summary>
+    [Export(typeof(IDataService))]
+    public class DataService : IDataService
+    {
+        private ObservableCollection<DataItem> model;
+
+        public ObservableCollection<DataItem> GetModel()
+        {
+            if (model == null)
+            {
+                model = new ObservableCollection<DataItem>();
+
+                for (int i = 0; i < 20; i++)
+                {
+                    model.Add(new DataItem() { Id = i, Name = string.Format( "Name {0}  ", i), Description = string.Format("Description {0}  ", i)});
+                }              
+            }
+
+            return model;
+        }
+    }
+}
---
+++ DataColumnVisibility/DataColumnVisibility/Services/IDataService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.ObjectModel;
+using DataColumnVisibility.Models;
+
+namespace DataColumnVisibility.Services
+{
+    /// <summary>
+    /// Data service interface.
+    /// </summary>
+    public interface IDataService
+    {
+        ObservableCollection<DataItem> GetModel();
+    }
+}

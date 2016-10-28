a3fca3f2acfe632a2cd456fff0a2c747dcc35ace
frankwwu <frankwwu@hotmail.com>
2016-10-28 19:25:10 -0400

    DataColumnVisibility

---
+++ DataColumnVisibility/DataColumnVisibility/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition.Hosting;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace DataColumnVisibility
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected CompositionContainer Container { get; set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            AggregateCatalog catalog = new AggregateCatalog();
+            catalog.Catalogs.Add(new AssemblyCatalog(System.Reflection.Assembly.GetExecutingAssembly()));
+
+            Container = new CompositionContainer(catalog);
+            this.Container.GetExportedValue<MainWindow>().Show();
+        }
+    }
+}

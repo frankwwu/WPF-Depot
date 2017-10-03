61a1c42f47f0f304f2be46d7d229c76ea1655351
frankwwu <frankwwu@hotmail.com>
2017-10-03 18:55:55 -0400

    DataGridCopy

---
+++ DataGridCopy/DataGridCopy/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿
+namespace DataGridCopy.Models
+{
+    public class Person
+    {
+        public string GivenName { get; set; }
+
+        public string Surname { get; set; }     
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+ 
+        public string State { get; set; }
+
+        public string Zip { get; set; }       
+    }
+}
---
+++ DataGridCopy/DataGridCopy/ViewModels/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using DataGridCopy.Models;
+using DataGridCopy.Services;
+
+namespace DataGridCopy.ViewModels
+{
+    [Export(typeof(ViewModel))]
+    public class ViewModel
+    {
+        private readonly IDataService _dataService;
+
+        [ImportingConstructor]
+        public ViewModel(IDataService dataService)
+        {
+            _dataService = dataService;
+            Persons = _dataService.Persons;
+        }
+
+        public List<Person> Persons { get; private set; }
+    }
+}

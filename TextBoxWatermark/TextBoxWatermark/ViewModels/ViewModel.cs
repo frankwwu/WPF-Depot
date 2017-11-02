fc9081f760dba50e5a3b0f774fbf75a456af4688
frankwwu <frankwwu@hotmail.com>
2017-11-01 20:04:18 -0400

    TextBox Watermark

---
+++ TextBoxWatermark/TextBoxWatermark/ViewModels/ViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.Composition;
+
+namespace TextBoxWatermark.ViewModels
+{
+    [Export(typeof(ViewModel))]
+    public class ViewModel
+    {
+        [ImportingConstructor]
+        public ViewModel()
+        {                    
+        }
+    }
+}

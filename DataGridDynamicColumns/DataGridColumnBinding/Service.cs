761ae8e9d6cb8e99fa0165dc91192aacf2e83f20
frankwwu <frankwwu@hotmail.com>
2021-07-21 18:17:43 -0400

    DataGridColumnBinding

---
+++ DataGridDynamicColumns/DataGridColumnBinding/Service.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.Composition;
+using System.Xml.Linq;
+
+namespace DataGridColumnBinding
+{
+    [Export(typeof(IService))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    public class Service : IService
+    {
+        public Service()
+        {
+        }
+
+        public DataGridModel Refresh()
+        {
+            XDocument doc = XDocument.Load("DataSet.xml");
+            return XmlConverters.ConvertXmlToDataTable(doc);
+        }
+    }
+}

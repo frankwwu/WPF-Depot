d6fff5b71e4b4d4b28dae82238ddfdde7df81c89
frankwwu <frankwwu@hotmail.com>
2024-01-27 13:02:52 -0500

    DataGridScrollToSelectedItemBehavior

---
+++ ScrollToSelectedItem/ScrollToSelectedItem/DataGridScrollToSelectedItemBehavior.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Windows.Controls;
+
+namespace ScrollToSelectedItem
+{
+    public class DataGridScrollToSelectedItemBehavior : Behavior<DataGrid>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            this.AssociatedObject.SelectionChanged += OnSelectionChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            this.AssociatedObject.SelectionChanged -= OnSelectionChanged;
+        }
+
+        void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is DataGrid)
+            {
+                DataGrid dataGrid = (sender as DataGrid);
+                if (dataGrid.SelectedItem != null)
+                {
+                    dataGrid.Dispatcher.BeginInvoke(
+                        (Action)(() =>
+                        {
+                            dataGrid.UpdateLayout();
+                            if (dataGrid.SelectedItem !=
+                            null)
+                                dataGrid.ScrollIntoView(
+                                dataGrid.SelectedItem);
+                        }));
+                }
+            }
+        }
+    }
+}
+

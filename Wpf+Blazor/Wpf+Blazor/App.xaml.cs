bc17429f901fcc27ef4dee6c62587d646daf0820
frankwwu <frankwwu@gmail.com>
2025-09-18 13:28:18 -0400

    Wpf+Blazor

---
+++ Wpf+Blazor/Wpf+Blazor/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace Wpf_Blazor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+    }
+
+}
---
+++ Wpf+Blazor/Wpf+Blazor/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace Wpf_Blazor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+        }
+    }
+}
